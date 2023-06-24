dd072d083a6e98a7f7cfed16a84e57eae314eb0c
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-24 11:07:39 +1000

    Completed scr-sw.js file with asset caching.

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,22 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === "script" ||
+    request.destination === "style" ||
+    request.destination === "font",
+  new CacheFirst({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+offlineFallback();
